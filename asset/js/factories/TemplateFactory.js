22ea2224ec490ebbe8790a8d13d7d1280b6af9c3
Thorekt <robinory77@gmail.com>
2022-06-10 13:27:10 +0200

    base

---
+++ asset/js/factories/TemplateFactory.js
@@ -0,0 +1,12 @@
+class TemplateFactory{
+    static getRecipeCardTemplate(){
+        const html = document.createElement('div');
+        html.classList.add('recipe-card');
+
+        const recipeImgZone = document.createElement('div');
+        recipeImgZone.classList.add 
+
+
+        return html;
+    }
+}
